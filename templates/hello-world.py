f219207967c3419e9247a2e15acef3ad8f61326a
Soumik Ghosh Moulic <22228314+Soumik0833@users.noreply.github.com>
2020-01-24 11:13:49 +0100

    Delete Obsolete Files

--- templates/hello-world.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-board = [['-','-','-'], ['-','-','-'], ['-','-','-']]
-#matrixed = np.matrix(board)
-for i in board:
-    for j in i:
-        print('-', end='')
-    print('')
-#print(board)
